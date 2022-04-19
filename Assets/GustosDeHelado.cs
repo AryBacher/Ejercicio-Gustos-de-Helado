ae54013e88beb6fea01e42aac11c67d81d6894ca
AryBacher <47170979@est.ort.edu.ar>
2022-04-19 08:52:32 -0300

    Finished

--- Assets/GustosDeHelado.cs
+++ Assets/GustosDeHelado.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class GustosDeHelado : MonoBehaviour
 {
     public string Gusto;
-    public int CantidadG;
-
-    int total;
+    public float CantidadG;
+    float total;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,21 +17,23 @@ public class GustosDeHelado : MonoBehaviour
 
         else
         {
-            total = (CantidadG / 1000) * 500;
 
             switch (Gusto)
             {
                 case "DDL":
                     Debug.Log("Usted pidió " + CantidadG + " gramos de dulce de leche");
+                    total = (CantidadG / 1000) * 500;
                     Debug.Log("El precio total es de $"+total);
                     break;
                 case "CHO":
                     Debug.Log("Usted pidió " + CantidadG + " gramos de chocolate");
+                    total = (CantidadG / 1000) * 500;
                     Debug.Log("El precio total es de $" + total);
                     break;
                 case "FRU":
                     CantidadG = (CantidadG / 100) * 90 ;
                     Debug.Log("Usted pidió " + CantidadG + " gramos de frutilla");
+                    total = (CantidadG / 1000) * 500;
                     Debug.Log("El precio total es de $" + total);
                     break;
                 default:
